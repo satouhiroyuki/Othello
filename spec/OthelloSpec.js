3565bd3bfed9e1eaf9b6abd8942d10ff91a9545f
佐藤博之 <sato_hiroyuki@satouhiroyukinoMacBook-Pro.local>
2018-07-02 00:41:26 +0900

    オセロの基盤を作成。ターンの交代と盤面に駒を置けるように対応した

--- spec/OthelloSpec.js
+++ spec/OthelloSpec.js
@@ -4,27 +4,44 @@
     //testCode
     describe('this test is Othello.js',function(){
         it('testCase', function(){
-            expect(testJs.testFunction).toBeDefined();
-            expect(testJs.testFunction()).toBe("Hello Othello");
+            expect(testJs.initBoard).toBeDefined();
+            expect(testJs.initBoard()).toBe(true);
         });
         it('testCase', function(){
-            expect(testJs.testStringValue).toBeDefined();
-            expect(testJs.testStringValue()).toBe("0.0.1");
-        });
-        it('testCase', function(){
-            expect(testJs.testVersion).toBeDefined();
-            expect(testJs.testVersion()).toBe("0.0.1");
-        });
-        it('testCase', function(){
-            expect(testJs.testStringValue).toBeDefined();
-            expect(testJs.testStringValue()).toBe("0.0.1");
+            expect(testJs.getBoard).toBeDefined();
+            expect(testJs.getBoard(0,0)).toBe(0);
         });
     });
+
     //testCode
-    describe('this test is Othello.js',function(){
+    describe('this test is set Player1',function(){
         it('testCase', function(){
-            expect(testJs.testStringValue).toBeDefined();
-            expect(testJs.testStringValue()).toBe("0.0.1");
+            expect(testJs.initBoard).toBeDefined();
+            expect(testJs.setBoard).toBeDefined();
+
+            expect(testJs.setBoard(3,3,1));
+            expect(testJs.getBoard(3,3)).toBe(1);
+        });
+    });
+
+    //testCode
+    describe('this test is set Player2',function(){
+        it('testCase', function(){
+            expect(testJs.initBoard).toBeDefined();
+            expect(testJs.setBoard).toBeDefined();
+
+            expect(testJs.setBoard(3,3,2));
+            expect(testJs.getBoard(3,3)).toBe(2);
+        });
+    });
+
+    //testCode
+    describe('this test is set Player2',function(){
+        it('testCase', function(){
+            expect(testJs.changePlayer).toBeDefined();
+
+            expect(testJs.changePlayer(2)).toBe(1);
+            expect(testJs.changePlayer(1)).toBe(2);
         });
     });
 })();
--- src/js/Othello.js
+++ src/js/Othello.js
@@ -1,19 +1,85 @@
 var testString;
+var othelloBoard;
+var othelloBoardRows;
 
-function testFunction(){
-    testString = "Hello Othello";
-    return testString;
-};
+//盤面をまっさらに初期化する
+function initBoard(){
+    var boardSize = 4;
 
-function testVersion(){
-    testString = "0.0.1";
-    return testString;
-};
-function testStringValue(){
-    return testString;
-};
+    othelloBoard = new Array(boardSize);
+
+    for(let y = 0; y < boardSize; y++) {
+        othelloBoard[y] = new Array(boardSize).fill(0);
+    }
+
+    return true;
+}
+
+//初期化した後にゲームができるように盤面にオセロを配置する
+function settingBoard(){
+
+}
+
+//盤面のオセロの状況を確認する
+//0:NONE 1:Player1 2:Player2
+function getBoard(col,row){
+    var board = othelloBoard[col];
+    return board[row];
+}
+
+function setBoard(col,row,player){
+    var board = othelloBoard[col];
+    board[row] = player;
+
+    if(player == 1){
+        var boardString = "○";
+    }
+    else if(player == 2){
+        var boardString = "●";
+    }
+
+    return boardString;
+}
+
+function changePlayer(player){
+    var nextPlayer = player;
+
+    if(player == 1){
+        nextPlayer = 2;
+    }
+    else if(player == 2){
+        nextPlayer = 1;
+    }
+
+    return nextPlayer;
+}
+
+//補助ファンクション：置ける場所の把握のために記述
+function setBoardItem(){
+    for(col=0;col< othelloBoard.length;col++){
+        var board = othelloBoard[col];
+        for(row=0;row<board.length;row++){
+            document.getElementById("td"+col+row).innerHTML=board[row];
+        }
+    }
+}
 
 //Jasmineのtestにはexportが必要になるらしい
-exports.testFunction = testFunction;
-exports.testVersion = testVersion;
-exports.testStringValue = testStringValue;
+exports.initBoard = initBoard;
+exports.getBoard = getBoard;
+exports.setBoard = setBoard;
+exports.changePlayer = changePlayer;
+
+//event
+function onbuttonClick(col,row){
+    var playerNo = document.getElementById("Player").innerHTML;
+
+    var boardString = setBoard(col,row,playerNo);
+    document.getElementById("btn"+col+row).innerHTML=boardString;
+
+    var nextPlayer = changePlayer(playerNo);
+    document.getElementById("Player").innerHTML=nextPlayer;
+
+    setBoardItem();
+}
+
