b6f0ea51974b90d5e6652aafb4162797287999e0
佐藤博之 <sato_hiroyuki@satouhiroyukinoMacBook-Pro.local>
2018-07-17 01:25:43 +0900

    サンプルコードを含めたサーバーの構築：実験

--- server/server.js
+++ server/server.js
@@ -1,18 +1,45 @@
 var http = require('http');
 var path = require('path');
+//var url = require('url');
 var fs = require('fs');
 
+var mineTypes = {
+  '.js': 'text/javascript',
+  '.html': 'text/html',
+  '.css': 'text/css'
+};
+
+/*
 var pages = [
     {route: '', output: 'Woohoo!'},
     {route: 'about', output: 'シンプルなサンプルコードです'},
     {route: 'another page' , output: function() {return 'これが'+this.route;}}
-]
+]*/
 
 http.createServer(function (request, response) {
+  if(request.url == '/favicon.ico'){
+    response.end();
+    return;
+  }
+  //var lookup = url.parse(decodeURI(request.url)).pathname;
+  //lookup = path.normalize(lookup);
+  //lookup = (lookup == '/') ? 'index.html' : lookup;
   var lookup = path.basename(decodeURI(request.url)) || 'index.html',
-  f = 'content/' + lookup;
+  f = '../' + lookup;
   fs.exists(f,function(exists){
-    console.log(exists ? lookup + "は、存在します。" : lookup + "はそんざいしません");
+    fs.readFile(f,function(err,data){
+      if(err){
+        console.log(f);
+        response.writeHead(500);
+        response.end('server Error!');
+        return;
+      }
+      var headers = {'Content-Type':mineTypes[path.extname(f)]};
+      console.log(f);
+      response.writeHead(200,headers);
+      response.end(data);
+    });
+    return;
   });
   /*
   pages.forEach(function(page){
