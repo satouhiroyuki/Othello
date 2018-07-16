4cc9bfee76b24cbcc4eeaf1ca22acf2396f89d1d
佐藤博之 <sato_hiroyuki@satouhiroyukinoMacBook-Pro.local>
2018-07-17 01:26:11 +0900

    サンプルコードの削除

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var path = require('path');
-//var url = require('url');
 var fs = require('fs');
 
 var mineTypes = {
@@ -9,21 +8,11 @@ var mineTypes = {
   '.css': 'text/css'
 };
 
-/*
-var pages = [
-    {route: '', output: 'Woohoo!'},
-    {route: 'about', output: 'シンプルなサンプルコードです'},
-    {route: 'another page' , output: function() {return 'これが'+this.route;}}
-]*/
-
 http.createServer(function (request, response) {
   if(request.url == '/favicon.ico'){
     response.end();
     return;
   }
-  //var lookup = url.parse(decodeURI(request.url)).pathname;
-  //lookup = path.normalize(lookup);
-  //lookup = (lookup == '/') ? 'index.html' : lookup;
   var lookup = path.basename(decodeURI(request.url)) || 'index.html',
   f = '../' + lookup;
   fs.exists(f,function(exists){
@@ -41,15 +30,4 @@ http.createServer(function (request, response) {
     });
     return;
   });
-  /*
-  pages.forEach(function(page){
-    if(page.route == lookup){
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.end(typeof page.output === 'function' ? page.output():page.output);
-    }
-  });
-  if(!response.finished){
-    response.writeHead(404);
-    response.end('ページが見つかりません！')
-  }*/
 }).listen(8080);
